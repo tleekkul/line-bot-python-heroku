fb524dc805270ed2fe37d7f91a3cdf44e5f9a6d9
Tle Ekkul <e.aryuth@gmail.com>
2017-12-19 13:24:47 +0700

    access via os env

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('') #Your Channel Access Token
-handler = WebhookHandler('') #Your Channel Secret
+line_bot_api = LineBotApi(os.environ['LINE_ACCESS_TOKEN']) #Your Channel Access Token
+handler = WebhookHandler(os.environ['LINE_SECRET']) #Your Channel Secret
 
 @app.route("/callback", methods=['POST'])
 def callback():
